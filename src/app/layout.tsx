791b986c3108532bed6d217f0478fab7ad8cdab8
Sam Cunion <krookaexpert@gmail.com>
2023-10-06 22:36:05 +0100

    Added game selector and credits viewer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,19 @@
+import "./app.css";
 export default function RootLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
+
     return (
         <html lang="en">
-            <body>
+            <body suppressHydrationWarning={true}>
                 {children}
             </body>
         </html>
     )
+}
+
+export const metadata = {
+    title: "Casual Pokémon Teambuilder"
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,33 @@
+"use client"
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ReactElement, useState } from "react";
+import GameSelector from "./GameSelector";
+import Credits from "./Credits";
+import InfoOverlay from "./InfoOverlay";
+
 export default function Page() {
-    return <h1>Hello World!</h1>
+    const [appState, changeAppState] = useState("game-select");
+    const [overlayActive, setOverlayActive] = useState(false);
+
+    function gameVersionSelect(selected_game) {
+        changeAppState("main");
+    }
+    
+    if (appState === "game-select") {
+        return (
+            <div id="page">
+                <GameSelector resolve={gameVersionSelect} />
+                <Credits overlayHandler={setOverlayActive} />
+                {overlayActive ? <InfoOverlay type={0} selfActive={setOverlayActive} /> : null}
+            </div>
+        )   
+    }
+    else {
+        return (
+            <div id="page">
+                <h1>New State</h1>
+                <Credits overlayHandler={setOverlayActive} />
+            </div>
+        )
+    }
 }
